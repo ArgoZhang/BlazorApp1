60949743fd3efa5ad9b3432318a211d506585494
Argo-Asicotech <argo@163.com>
2023-01-09 18:49:21 +0800

    doc: 增加嵌套组件生命周期

---
+++ BlazorApp1/Components/Foo2Component.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorApp1.Components;
+
+public partial class Foo2Component
+{
+    [CascadingParameter]
+    [NotNull]
+    public List<string>? Foos { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Foos.Add("1");
+    }
+}
---
+++ BlazorApp1/Pages/Foo2.razor.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BlazorApp1.Pages;
+
+public partial class Foo2
+{
+    private List<string> Foos { get; } = new();
+}
