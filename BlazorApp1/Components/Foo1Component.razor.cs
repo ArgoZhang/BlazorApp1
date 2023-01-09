18bfe4964ba6b9781ce34602181aabfc7f5d6846
Argo-Asicotech <argo@163.com>
2023-01-09 18:49:01 +0800

    doc: 增加组件生命周期

---
+++ BlazorApp1/Components/Foo1Component.razor.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorApp1.Components;
+
+public partial class Foo1Component
+{
+    [Parameter]
+    [NotNull]
+    public List<string> Items { get; set; } = new();
+
+    private string BeginTime { get; } = DateTime.Now.ToString();
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Thread.Sleep(1000);
+        Items.Add($"FooComponent: OnInitialized {DateTime.Now}");
+    }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        Thread.Sleep(1000);
+        Items.Add($"FooComponent: OnParametersSet {DateTime.Now}");
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+
+        Thread.Sleep(1000);
+        Items.Add($"FooComponent: OnAfterRender {DateTime.Now}");
+
+        if (firstRender)
+        {
+            StateHasChanged();
+        }
+    }
+}
---
+++ BlazorApp1/Pages/Foo1.razor.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BlazorApp1.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorApp1.Pages;
+
+public partial class Foo1
+{
+    private List<string> Items { get; } = new();
+
+    private string BeginTime { get; } = DateTime.Now.ToString();
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Thread.Sleep(1000);
+        Items.Add($"Foo: OnInitialized {DateTime.Now}");
+    }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        Thread.Sleep(1000);
+        Items.Add($"Foo: OnParametersSet {DateTime.Now}");
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        base.OnAfterRender(firstRender);
+
+        Thread.Sleep(1000);
+        Items.Add($"Foo: OnAfterRender {DateTime.Now}");
+
+        if (firstRender)
+        {
+            StateHasChanged();
+        }
+    }
+}
