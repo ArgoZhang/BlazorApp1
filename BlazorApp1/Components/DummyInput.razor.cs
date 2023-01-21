cae03625d12fdcd9d7991eee54fef01c8531c60e
Argo Zhang <argo@163.com>
2023-01-21 13:31:46 +0800

    refactor: 双向绑定示例

---
+++ BlazorApp1/Components/DummyInput.razor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components;
+
+public partial class DummyInput
+{
+    [Parameter]
+    public string? Value { get; set; }
+
+    [Parameter]
+    public EventCallback<string?> ValueChanged { get; set; }
+
+    private string? ValueString
+    {
+        get { return Value; }
+        set
+        {
+            Value = value;
+            if (ValueChanged.HasDelegate)
+            {
+                ValueChanged.InvokeAsync(Value);
+            }
+        }
+    }
+}
