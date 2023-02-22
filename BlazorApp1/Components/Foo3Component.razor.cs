dadec2cd417344bece5c96e019064ef0c178ebc4
Argo Zhang <argo@live.ca>
2023-02-22 18:31:33 +0800

    feat: 增加级联参数

---
+++ BlazorApp1/Components/Foo3Component.razor.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components;
+
+public partial class Foo3Component
+{
+    [CascadingParameter]
+    private List<string>? Items { get; set; }
+}
---
+++ BlazorApp1/Pages/Foo3.razor.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages;
+
+public partial class Foo3
+{
+    private List<string> Items { get; set; } = new(50) { "12" };
+
+    public EventCallback Test { get; set; }
+
+    private void OnClick()
+    {
+        Items.Add(DateTime.Now.ToString());
+    }
+}
