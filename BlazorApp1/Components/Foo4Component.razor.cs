d3565feac4a5a9244dfab488f212de3cdc87fef4
Argo Zhang <argo@live.ca>
2023-02-23 23:59:50 +0800

    feat: 增加 Foo4 组件

---
+++ BlazorApp1/Components/Foo4Component.razor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components;
+
+public partial class Foo4Component
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public RenderFragment<WeatherForecast>? HeaderTemplate { get; set; }
+
+    [Parameter]
+    public RenderFragment? FooterTemplate { get; set; }
+}
