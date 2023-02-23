253dfc798c78853d2a7f634a0bebe3d2e2d286d2
Argo Zhang <argo@live.ca>
2023-02-24 00:00:00 +0800

    feat: 增加 Foo5 组件

---
+++ BlazorApp1/Components/Foo5Component.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorApp1.Components;
+
+public class Foo5Component : ComponentBase
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(1, "div");
+        builder.AddContent(2, "我是代码生成");
+        builder.CloseElement();
+    }
+}
