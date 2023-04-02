f7d4d45cabe9e231dcc9ac6b039586b6691b5d4e
Argo Zhang <argo@live.ca>
2023-04-02 10:47:17 +0800

    feat: 增加 RenderFragment 高级篇

---
+++ BlazorApp1/Components/Foo6Component.razor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorApp1.Components;
+
+public partial class Foo6Component
+{
+    [Parameter]
+    public RenderFragment<RenderFragment>? Template { get; set; }
+
+    [Parameter]
+    [NotNull]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        ChildContent ??= new RenderFragment(builder =>
+        {
+            builder.AddContent(0, "");
+        });
+    }
+}
